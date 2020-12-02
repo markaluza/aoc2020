5391589044d1ec40283e07f8703bf396587c4392
kaluza_marek <kaluza@adash.cz>
2020-12-02 08:01:05 +0100

    ukol2

--- aoc2020/Program.cs
+++ aoc2020/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
 namespace Aoc2020
 {
 
@@ -47,9 +47,9 @@ namespace Aoc2020
             Console.WriteLine("Values were not found !!!!!");
         }
 
-        static void p_2a()
+        static void p_1b()
         {
-            Console.WriteLine("AOC2020_2a");
+            Console.WriteLine("AOC2020_1b");
 
             var numbers = GetInput();
 
@@ -72,12 +72,117 @@ namespace Aoc2020
 
         }
 
+        struct Policy
+        {
+            public int a;
+            public int b;
+
+            public char letter;
+
+            public string password;
+        }
+        static List<Policy> GetInput_2()
+        {
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =  new System.IO.StreamReader(@"..\input_2.i");
+
+            List<Policy> policies = new List<Policy>();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                
+                var parts = line.Split(':', ' ', '-'); // 9-12 q: qqqxhnhdmqqqqjz
+
+                if (parts.Length != 5)
+                {
+                    Debug.Assert(false);
+                    continue;
+                }
+
+                Policy p = new Policy();
+                p.a = Convert.ToInt32(parts[0]);
+                p.b = Convert.ToInt32(parts[1]);
+                p.letter =  Convert.ToChar(parts[2]);
+                p.password = parts[4];
+
+                policies.Add(p);
+
+            }
+            return policies;
+        }
+
+
+
+        static bool EvalPassword1(Policy policy)
+        {
+            int cntr = 0;
+            
+            foreach (char ch in policy.password)
+            {
+                if (ch == policy.letter)
+                cntr++;
+            }
+
+            return cntr >= policy.a && cntr <= policy.b;
+        }
+
+
+       static void p_2a()
+        {
+            Console.WriteLine("AOC2020_2a");
+
+            var policies = GetInput_2();
+
+            int valid = 0;
+
+            foreach (Policy p in policies)
+            {
+                if (EvalPassword1(p))
+                    valid++;
+            }
+
+            Console.WriteLine("Vlid passwords : {0}", valid);
+
+        }
+
+        static bool EvalPassword2(Policy policy)
+        {
+            int cntr = 0;
+            
+            if (policy.password[policy.a-1] == policy.letter) cntr++;
+            if (policy.password[policy.b-1] == policy.letter) cntr++;
+
+            return cntr == 1;
+        }
+        
+       static void p_2b()
+        {
+            Console.WriteLine("AOC2020_2b");
+
+            var policies = GetInput_2();
+
+            int valid = 0;
+
+            foreach (Policy p in policies)
+            {
+                if (EvalPassword2(p))
+                    valid++;
+            }
+
+            Console.WriteLine("Vlid passwords : {0}", valid);
+
+        }
 
 
         static void Main(string[] args)
         {
             //p_1a();
-            p_2a();
+            //p_1b();
+
+            //p_2a();
+            p_2b();
 
             Console.ReadKey();
             return;
