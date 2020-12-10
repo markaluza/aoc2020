de31996272bd495fa255b2066f1c8fd0cd06aa92
kaluza_marek <kaluza@adash.cz>
2020-12-10 11:36:06 +0100

    vylepseni...

--- aoc2020/day10/results.cs
+++ aoc2020/day10/results.cs
@@ -5,15 +5,23 @@ namespace Aoc2020
 {
     class Day10
     {
-        public static void Task1()
-        {
-            Console.WriteLine("AOC2020_Day10_Task1");
 
-            var input = Day1_Common.GetIntInput(@"./day10/input.txt");
+        private static List<int> GetInput()
+        {
+            var input =  Day1_Common.GetIntInput(@"./day10/input.txt");
 
             input.Add(0);
             input.Sort();
             input.Add(input[input.Count -1] + 3);
+
+            return input;
+        }
+
+        public static void Task1()
+        {
+            Console.WriteLine("AOC2020_Day10_Task1");
+
+            var input = GetInput();
 
             Dictionary<int, int> difs = new Dictionary<int, int>();
 
@@ -34,38 +42,33 @@ namespace Aoc2020
         {
             Console.WriteLine("AOC2020_Day10_Task2");
 
-            var input = Day1_Common.GetIntInput(@"./day10/input.txt");
+            var input = GetInput();
 
-            input.Add(0);
-            input.Sort();
-            input.Add(input[input.Count -1] + 3);
-
-            List<int> grps = new List<int> ();
-
-            int grpnumbers = 0;
+            int nmbs = 0;
             Int64 mult = 1;
 
             for (int i= 1; i < input.Count -1; i++)
             {
                 if (input[i+1] - input[i-1] == 2)
                 {
-                    grpnumbers++;
+                    nmbs++;
                 }
                 else
                 {
-                    switch(grpnumbers)
+                    switch(nmbs)
                     {
+                        case 0: break;
                         case 1: mult *=2; break;
                         case 2: mult *=4; break;
                         case 3: mult *=7; break;
+                        default: throw new Exception("dopsat...");
                     }                    
-                    grpnumbers = 0;
+                    nmbs = 0;
                 }
             } 
-             Console.WriteLine("mult : {0}",  mult);
 
+            Console.WriteLine("mult : {0}",  mult);
         }        
-
 
     }
 }
