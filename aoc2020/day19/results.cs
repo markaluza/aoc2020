3def4c553cf8d1b2a7d318d23042b938d8121c50
kaluza_marek <kaluza@adash.cz>
2020-12-21 13:27:49 +0100

    day 19 p2

--- aoc2020/day19/results.cs
+++ aoc2020/day19/results.cs
@@ -54,22 +54,35 @@ namespace Aoc2020
 
                 string rl = " " + foo[1] + " ";
 
-                /*int srhc = rl.IndexOf(" " + foo[0] + " ");
+                int srhc = rl.IndexOf(" " + foo[0] + " ");
 
                 if (srhc > 0)
                 {
 
+                    // grrr nevim jak udelat rekurzi v regexu :( tak to proste zopakuju 1 - 10x
                    if (foo[0] == "11")
                    {
-                       rl = "42 31 | ( 42 (?1)? 31 ) " ;
-                   };
+                       rl = "";
+                       for (int i = 1; i < 10; i++)
+                       {
+
+                        string s2 = "";
+                        for (int j = 1; j <= i ; j++)
+                        {    
+                           s2 = " 42 " + s2 + " 31 ";
+                        }
+
+                        if (rl != "") rl += " | ";
+                        rl += s2;
+                       }
+                   }
 
                    if (foo[0] == "8")
                    {
                        rl = " ( 42 )+ ";
                    };
 
-                }*/
+                }
 
                 rules.Add(foo[0],  rl);
             }
@@ -120,9 +133,8 @@ namespace Aoc2020
         public static void Task2()
         {
             
-            Console.WriteLine("AOC2020_Day19_2 -- !!! not ready"); 
+            Console.WriteLine("AOC2020_Day19_2 "); 
 
-            /*
             
 
             var lines = System.IO.File.ReadAllLines(@"./day19/input.txt");
@@ -156,7 +168,7 @@ namespace Aoc2020
 
             }
 
-            Console.WriteLine("Valid : {0}", validstrings); */
+            Console.WriteLine("Valid : {0}", validstrings); 
 
 
         }
