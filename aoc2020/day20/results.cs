825d71063b837bcc865f87db86514dbff2229878
kaluza_marek <kaluza@adash.cz>
2021-01-05 16:52:37 +0100

    day 20 p2

--- aoc2020/day20/results.cs
+++ aoc2020/day20/results.cs
@@ -5,6 +5,76 @@ using System.Text.RegularExpressions;
 namespace Aoc2020
 {
 
+    class Arr2dManipulation
+    {
+        static public void FlipH(ref char [,] Arr)
+        {
+            int dx = Arr.GetLength(0);
+            int dy = Arr.GetLength(1);
+
+            var Arr2 = Arr;
+            Arr = new char[dx,dy];
+            for(int y =0 ; y < dy; y++)
+            {
+                for (int x = 0; x < dx; x++)
+                {
+                    Arr[x,y] = Arr2[dx-1-x,y];
+                }
+            }
+        }
+        
+        public static void Rotate(ref char [,] Arr)
+        {
+
+            int dx = Arr.GetLength(0);
+            int dy = Arr.GetLength(1);
+
+            var Arr2 = Arr;
+            Arr = new char[dy,dx];
+
+               
+            for(int y =0 ; y < dy; y++)
+            {
+                for (int x = 0; x < dx; x++)
+                {
+                    Arr[y,x] = Arr2[dx-1-x ,y];
+                }
+            }
+        }     
+
+        public static int GetHaspCount(char [,] Arr)
+        {
+
+            int dx = Arr.GetLength(0);
+            int dy = Arr.GetLength(1);
+
+            int hc = 0;
+            for(int y =0 ; y < dy; y++)
+            {
+                for (int x = 0; x < dx; x++)
+                {
+                    if (Arr[x,y] == '#') hc++;
+                }
+            }
+            return hc;
+        }      
+
+        public static void Print(char [,] Arr)
+        {
+            for(int y =0; y < Arr.GetLength(1); y++)
+            {
+                for(int x =0; x < Arr.GetLength(0); x++)
+                {
+                    Console.Write(Arr[x,y]);
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("----");
+
+        }
+
+    }
+
     class Tile
     {
         public int TileNum = -1;
@@ -18,7 +88,7 @@ namespace Aoc2020
             return new string( charArray );
         }
 
-        char [,] map = new char[10,10];
+        public char [,] map = new char[10,10];
 
         public string l { get { string foo = ""; for (int i =0; i < 10; i++) foo += map[0,i]; return foo; } }
         public string r { get  { string foo = ""; for (int i =0; i < 10; i++) foo += map[9,i]; return foo; } }        
@@ -28,10 +98,7 @@ namespace Aoc2020
         public void SetData(string tiledata)
         {
             var lines = tiledata.Split("\r\n");
-
             TileNum = int.Parse(lines[0].Split(" ")[1].TrimEnd(':'));
-
-
             for(int y =1 ; y <= 10; y++)
             {
                 for (int x = 0; x < 10; x++)
@@ -40,131 +107,60 @@ namespace Aoc2020
                 }
 
             }
-
-            Sides.Add(t);
-            Sides.Add(r);
-            Sides.Add(ReverseString(b));
-            Sides.Add(ReverseString(l));             
-
+            Sides.Add(t);  Sides.Add(r);  Sides.Add(ReverseString(b)); Sides.Add(ReverseString(l));             
         }        
 
-        public void Rotate()
-        {
-            var map2 = map;
-            map = new char[10,10];
-            for(int y =0 ; y < 10; y++)
-            {
-                for (int x = 0; x < 10; x++)
-                {
-                    map[x,y] = map2[y,9 - x];
-                }
-            }
-        }
+        public void Rotate() { Arr2dManipulation.Rotate(ref map); }
 
-        public void FlipH()
-        {
-            var map2 = map;
-            map = new char[10,10];
-            for(int y =0 ; y < 10; y++)
-            {
-                for (int x = 0; x < 10; x++)
-                {
-                    map[x,y] = map2[9-x,y];
-                }
-            }
-        }
-        public void FlipV()
-        {
-            var map2 = map;
-            map = new char[10,10];
-            for(int y =0 ; y < 10; y++)
-            {
-                for (int x = 0; x < 10; x++)
-                {
-                    map[x,y] = map2[x,9-y];
-                }
-            }
-        }
-
-
+        public void FlipH() { Arr2dManipulation.FlipH(ref map); }
 
         public void MakeTopside(string side)
         {
-            // proste nahodne rotuje a flipuji tak dlouho at zo padne...
+            // proste nahodne rotuje a flipuji tak dlouho az to padne...
             for (int i =0; ; i++)
             {
-                if ((i+1)%3 == 0) FlipH();
-                if ((i+1)%5 == 0) FlipV();
-
-                if (t != side) Rotate();
-                else
-                {
-                    return;
-                }
+                if (t == side) return;
+                if ((i+1)%4 == 0) FlipH();
+                else Rotate();
             }
         }
 
         public void MakeLeftside(string side)
         {
-            // proste nahodne rotuje a flipuji tak dlouho at zo padne...
+            // proste nahodne rotuje a flipuji tak dlouho az to padne...
             for (int i =0; ; i++)
             {
-                if ((i+1)%3 == 0) FlipH();
-                if ((i+1)%5 == 0) FlipV();
-
-                if (l != side) Rotate();
-                else
+                if (l == side) 
                 {
                     return;
                 }
+                if ((i+1)%4 == 0) FlipH();
+                else Rotate();
             }
         }
-
-
 
         public bool ContainsSide(string side)
         {
             return Sides.Contains(side) || Sides.Contains(ReverseString(side));
         }
 
-        public Tile GetPossibleNeighbour_onside(TileList list, string side)
-        {
-            foreach(var tile in list)
-            {
-                if (TileNum == tile.TileNum) continue;
-                if (tile.ContainsSide(side)) { return tile; }
-                
-            }
-            return null;
-        }        
-
         public int GetPossibleNeighbours(TileList list)
         {
-
             int neighbours = 0;
-
             foreach(var tile in list)
             {
                 if (TileNum == tile.TileNum) continue;
-
                 foreach (var side in Sides)
                 {
-                    if (tile.ContainsSide(side)) 
-                    {
-                        neighbours++;
-                    }
+                    if (tile.ContainsSide(side)) neighbours++;
                 }
-                
             }
-
             return neighbours;
         }
-
     }
 
     class TileList : List<Tile>
     {
-        
         public void Load()
         {
 
@@ -177,7 +173,6 @@ namespace Aoc2020
                 t.SetData(tiledata);
                 Add(t);
             }
-
         }
 
         public TileList Clone()
@@ -188,7 +183,6 @@ namespace Aoc2020
                 Add(tile);
             }
             return foo;
-
         }
 
         public Tile FindMatchingTileBySide(Tile Orig, string side)
@@ -198,15 +192,10 @@ namespace Aoc2020
             {
                 if (tile.TileNum != Orig.TileNum && tile.ContainsSide(side))
                 {
-                    if (ret != null)
-                    {
-                        // pozn test jestli nejsou unikatni ???
-                        System.Diagnostics.Debug.Assert(false);
-                    }
                     ret = tile;
+                    break;
                 }
             }
-
             return ret;
         }
 
@@ -218,7 +207,6 @@ namespace Aoc2020
 
             return null;
         } 
-
     }
 
 
@@ -228,26 +216,95 @@ namespace Aoc2020
         private static List<Tile> GetCorners(TileList list)
         {
             List<Tile> corners = new List<Tile>();
-
             foreach(var tile in list)
             {
                 int neighbours = tile.GetPossibleNeighbours(list);
-                if (neighbours == 2 )
+                if (neighbours == 2 ) corners.Add(tile);
+            }
+            return corners;
+        }
+        
+        class Monster
+        {
+            char [,] pattern = null;
+
+            int patternhashCount = 0;
+
+            public void Load()
+            {
+                pattern = new char[20, 3];
+                patternhashCount = 0;
+                var lines = System.IO.File.ReadAllLines(@"./day20/monster.txt");
+                for (int l = 0; l < 3; l++)
                 {
-                    corners.Add(tile);
+                    for (int ch=0; ch < 20; ch++)
+                    {
+                        pattern[ch, l] = lines[l][ch];
+                        if (pattern[ch,l] == '#') patternhashCount++;
+                    }
+                }
+
+            }
+
+            public void Rotate() { Arr2dManipulation.Rotate(ref pattern); }
+
+            public void FlipH() { Arr2dManipulation.FlipH(ref pattern); }            
+
+            int px { get { return pattern.GetLength(0); } }
+            int py { get { return pattern.GetLength(1); } }
+
+            bool IsMonster(int mx, int my, ref char [,] map)
+            {
+                for (int y = 0; y < py; y++)
+                {
+                    for (int x = 0; x < px; x++)
+                    {
+                        if (pattern[x,y] == '#')
+                        {
+                            if (map[mx+x, my + y] == '.') 
+                                return false;
+                        } 
+                    }
+                }
+                return true;
+            }
+
+            void SetMonster(int mx, int my, ref char [,] map)
+            {
+                for (int y = 0; y < py; y++)
+                {
+                    for (int x = 0; x < px; x++)
+                    {
+                        if (pattern[x,y] == '#') 
+                            map[mx+x, my + y] = 'O';
+                    }
                 }
             }
 
-            return corners;
+            
+            public void TestMonsters(ref char [,] map)
+            {
+                int mx = map.GetLength(0);
+                int my = map.GetLength(1);
 
+                // pres vsechny body
+                for (int y =0; y < my - py; y++)
+                {
+                    for (int x =0; x < mx - px; x++)
+                    {
+                        if (IsMonster(x,y, ref map))
+                        {
+                            SetMonster(x, y, ref map);
+                        }
+                    }
+                }
+            }
         }
-        
 
         public static void Task1()
         {
 
             Console.WriteLine("AOC2020_Day20_1"); 
-
 
             TileList list = new TileList();
             list.Load();
@@ -271,8 +328,10 @@ namespace Aoc2020
             TileList list = new TileList();
             list.Load();
 
+
             // proste vemu prvni roh
             var TopLeft = GetCorners(list)[0];
+
 
             // zrotuji jej tak aby mel vlevo a vpravo volno...
             while (
@@ -280,28 +339,59 @@ namespace Aoc2020
                 list.FindMatchingTileBySide(TopLeft, TopLeft.l) != null
             ) TopLeft.Rotate();
             
+            // mapa spravne zorientocanych tilu
+            Tile [,] tmap = new Tile[12,12];
+            tmap[0,0] = TopLeft;
 
-            Tile [,] map = new Tile[12,12];
-            map[0,0] = TopLeft;
-
+            int cnt = Convert.ToInt32(Math.Sqrt(list.Count));
             // vyplnim mapu 
-            for (int j =0; j < 12; j++)
+            for (int j =0; j < cnt; j++)
             {
-                for (int i =1; i < 12; i++)
+                for (int i =1; i < cnt; i++)
                 {
                     // najdu tile pod nim a zrotuji tak aby byl side na vrsku...
-                    Tile top = map[j, i-1];
-                    map[j, i] = list.FindMatchingTileBySide(top, top.b);
-                    map[j, i].MakeTopside(top.b);
+                    Tile top = tmap[j, i-1];
+                    tmap[j, i] = list.FindMatchingTileBySide(top, top.b);
+                    tmap[j, i].MakeTopside(top.b);
 
                     // najdu tile mapravo a zrotuji tak aby byl side na vnalevo
-                    Tile left = map[i-1, j];
-                    map[i, j] = list.FindMatchingTileBySide(left, left.r);
-                    map[i, j].MakeLeftside(left.r);
+                    Tile left = tmap[i-1, j];
+                    tmap[i, j] = list.FindMatchingTileBySide(left, left.r);
+                    tmap[i, j].MakeLeftside(left.r);
                 }
             }
 
-            Console.WriteLine("TODO...");
+            // jedna mapa
+            var map = new char[cnt*8, cnt * 8];
+            for (int ty =0; ty < cnt; ty++)
+            {
+                for (int tx =0; tx < cnt; tx++)
+                {
+                    // najdu tile pod nim a zrotuji tak aby byl side na vrsku...
+                    Tile t = tmap[tx, ty];
+
+                    for (int y =1; y < 9; y++)
+                    {
+                        for (int x = 1; x < 9; x++)
+                        {
+                            map[tx * 8 + x -1, ty*8 + y -1] = t.map[x,y];
+                        } 
+                    }
+                }
+            }
+
+            var monster = new Monster();
+            monster.Load();
+
+            for (int i =0; i < 8; i++)
+            {
+                monster.TestMonsters(ref map);
+                if ((i+1)%4 == 0) Arr2dManipulation.FlipH(ref map);
+                else Arr2dManipulation.Rotate(ref map);
+            }
+
+            Console.WriteLine("# : {0}", Arr2dManipulation.GetHaspCount(map));
+            
 
         }        
 
